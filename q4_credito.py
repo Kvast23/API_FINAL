0c19bd53af01d32e59efe96c56db3ef0b0133e51
Kvast23 <155157101+Kvast23@users.noreply.github.com>
2025-07-28 16:37:03 -0300

    q4_credito.py

--- q4_credito.py
+++ q4_credito.py
@@ -2,24 +2,24 @@ def main():
     print("=== Avaliação de Risco de Crédito ===")
     renda = input("Renda mensal (em R$): ")
     score = input("Histórico de crédito (0 a 1000): ")
-    risco = classificar_risco_credito(renda, score)
+    risco = classificao_risco_credito(renda, score)
     print("Resultado:", risco)
 
-def classificar_risco_credito(renda_mensal, historico_credito_score):
+def classificao_risco_credito(renda_mensal, score_credito):
     try:
         renda_mensal = float(renda_mensal)
-        historico_credito_score = int(historico_credito_score)
+         score_credito = int(score_credito)
     except:
-        return "Dados inválidos"
+        return "Dados não aceitos"
 
-    if historico_credito_score >= 800 and renda_mensal >= 5000:
+    if score_credito >= 800 and renda_mensal >= 5000:
         return "Risco Muito Baixo"
-    elif historico_credito_score >= 700 and renda_mensal >= 3000:
-        return "Risco Baixo"
-    elif (500 <= historico_credito_score <= 699 and renda_mensal >= 2000) or renda_mensal >= 4000:
+    elif score_credito >= 700 and renda_mensal >= 3000:
+        return "Baixo Risco "
+    elif (500 <= score_credito <= 699 and renda_mensal >= 2000) or renda_mensal >= 4000:
         return "Risco Moderado"
-    elif 300 <= historico_credito_score <= 499 or (1000 <= renda_mensal <= 1999):
-        return "Risco Alto"
+    elif 300 <= score_credito <= 499 or (1000 <= renda_mensal <= 1999):
+        return " Alto Risco"
     else:
         return "Risco Muito Alto"
 
